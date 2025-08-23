640c493bf14e5c791cd300b064ea27c7840ddc70
Mane Omen <mane@omen.com>
2025-08-23 14:48:49 +0200

    initial commit

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import {StrictMode} from 'react'
 import {createRoot} from 'react-dom/client'
 import App from './App.tsx'
 import {ChakraProvider} from "@chakra-ui/react";
-import {chakraThemeConfig} from "./core/configs/chakraThemeConfig.ts";
+import {chakraThemeConfig} from "./core/ui/configs/chakraThemeConfig.ts";
 
 createRoot(document.getElementById('root')!).render(
     <StrictMode>
