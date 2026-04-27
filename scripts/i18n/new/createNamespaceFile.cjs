390a6232754511ddb8561252fd6e0c1e738b8ee5
Mane Omen Ubuntu <tomevmarjan33@gmail.com>
2026-04-27 14:03:29 +0200

    fix: scripts

--- scripts/i18n/new/createNamespaceFile.cjs
+++ scripts/i18n/new/createNamespaceFile.cjs
@@ -2,12 +2,20 @@ const fs = require("fs");
 const path = require("path");
 
 module.exports = function createNamespaceFile(ROOT, namespace) {
-  const file = path.join(ROOT, "src/i18n/locales/en", `${namespace}.json`);
+  const localesDir = path.join(ROOT, "src/i18n/locales");
 
-  if (!fs.existsSync(file)) {
-    fs.writeFileSync(file, "{}\n");
-    console.log(`✓ Created ${namespace}.json`);
-  } else {
-    console.log(`⚠ ${namespace}.json already exists`);
+  const langs = fs
+    .readdirSync(localesDir, { withFileTypes: true })
+    .filter((d) => d.isDirectory())
+    .map((d) => d.name);
+
+  for (const lang of langs) {
+    const file = path.join(localesDir, lang, `${namespace}.json`);
+    if (!fs.existsSync(file)) {
+      fs.writeFileSync(file, "{}\n");
+      console.log(`✓ Created ${namespace}.json for '${lang}'`);
+    } else {
+      console.log(`⚠ ${namespace}.json already exists for '${lang}'`);
+    }
   }
 };
